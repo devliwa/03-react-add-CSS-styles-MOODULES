0a31cb3b5afee03873cae822beaec19c2e98cce7
Charles Liwa <charles.liwananda@gmail.com>
2025-09-06 18:17:35 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// HOW TO STYLE REACT COMPONENTS WITH CSS
+//--------------------------------
+// (not including external frameworks or prepocessors )
+
+// 1. EXTERNAL 
+// 2. MOODULES
+// 3. INLINE
+
+import Button from './Button/Button.jsx'
+
+function App() {
+
+    return(<Button />);
+}
+
+export default App
